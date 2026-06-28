61971a49c23c5036bf60fb4b134e1a2d825ebf19
ayushporwal1122-igtm <ayushporwal1122@gmail.com>
2026-06-28 18:00:13 +0530

    Added report download features

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from reportlab.lib import colors
+import pandas as pd
+from flask import send_file
+import os
+from werkzeug.utils import secure_filename
 from flask import Flask, render_template, request, redirect
+from datetime import date
 import mysql.connector
 
 app = Flask(__name__)
@@ -7,7 +14,7 @@ db = mysql.connector.connect(
     host="localhost",
     user="root",
     password="Ayush@888",
-    database="smart_attendence_db"
+    database="smart_attendance_db"
 )
 
 cursor = db.cursor()
@@ -22,13 +29,53 @@ def login():
 
 @app.route("/dashboard")
 def dashboard():
-    return render_template("dashboard.html")
 
+    # Total Students
+    cursor.execute("SELECT COUNT(*) FROM students")
+    total_students = cursor.fetchone()[0]
+
+    # Present Today
+    cursor.execute("""
+        SELECT COUNT(*)
+        FROM attendance
+        WHERE attendance_date = CURDATE()
+        AND status = 'Present'
+    """)
+    present_today = cursor.fetchone()[0]
+
+    # Absent Today
+    cursor.execute("""
+        SELECT COUNT(*)
+        FROM attendance
+        WHERE attendance_date = CURDATE()
+        AND status = 'Absent'
+    """)
+    absent_today = cursor.fetchone()[0]
+
+    # Today's Date
+    current_date = date.today()
+
+    return render_template(
+        "dashboard.html",
+        total_students=total_students,
+        present_today=present_today,
+        absent_today=absent_today,
+        current_date=current_date
+    )
 @app.route("/add_student", methods=["GET", "POST"])
 def add_student():
 
     if request.method == "POST":
+        photo = request.files["photo"]
 
+        filename = ""
+
+        if photo.filename != "":
+            filename = secure_filename(photo.filename)
+
+            upload_folder = os.path.join(app.root_path, "static", "uploads")
+            os.makedirs(upload_folder, exist_ok=True)
+            photo.save(os.path.join(upload_folder, filename))
         student_name = request.form["student_name"]
         roll_number = request.form["roll_number"]
         class_name = request.form["class_name"]
@@ -40,11 +87,11 @@ def add_student():
 
         sql = """
         INSERT INTO students
-        (student_name, roll_number, class_name, section, father_name, mobile, email, address)
-        VALUES (%s,%s,%s,%s,%s,%s,%s,%s)
+        (student_name, roll_number, class_name, section, father_name, mobile, email, address, photo)
+        VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)
         """
 
-        values = (student_name, roll_number, class_name, section, father_name, mobile, email, address)
+        values = (student_name, roll_number, class_name, section, father_name, mobile, email, address, filename)
 
         cursor.execute(sql, values)
         db.commit()
@@ -56,11 +103,36 @@ def add_student():
 @app.route("/view_students")
 def view_students():
 
-    cursor.execute("SELECT * FROM students")
+    search = request.args.get("search")
+
+    if search:
+
+        sql = """
+        SELECT * FROM students
+        WHERE student_name LIKE %s
+        OR roll_number LIKE %s
+        """
+
+        value = ("%" + search + "%", "%" + search + "%")
+
+        cursor.execute(sql, value)
+
+    else:
+
+        cursor.execute("SELECT * FROM students")
 
     students = cursor.fetchall()
 
     return render_template("view_students.html", students=students)
+
+@app.route("/attendance")
+def attendance():
+
+    cursor.execute("SELECT * FROM students")
+
+    students = cursor.fetchall()
+
+    return render_template("attendance.html", students=students)
 
 @app.route("/edit_student/<int:id>", methods=["GET", "POST"])
 def edit_student(id):
@@ -111,6 +183,149 @@ def edit_student(id):
     student = cursor.fetchone()
 
     return render_template("edit_student.html", student=student)
+
+
+@app.route("/save_attendance", methods=["POST"])
+def save_attendance():
+
+    student_ids = request.form.getlist("student_id")
+
+    for student_id in student_ids:
+
+        status = request.form.get(f"attendance_{student_id}")
+
+        sql = """
+        INSERT INTO attendance (student_id, attendance_date, status)
+        VALUES (%s, CURDATE(), %s)
+        """
+
+        cursor.execute(sql, (student_id, status))
+
+    db.commit()
+
+    return redirect("/attendance")
+
+@app.route("/reports", methods=["GET", "POST"])
+def reports():
+
+    if request.method == "POST":
+
+        attendance_date = request.form["attendance_date"]
+
+        sql = """
+        SELECT
+            students.student_name,
+            students.roll_number,
+            attendance.attendance_date,
+            attendance.status
+        FROM attendance
+        INNER JOIN students
+        ON attendance.student_id = students.id
+        WHERE attendance.attendance_date = %s
+        ORDER BY attendance.attendance_date DESC
+        """
+
+        cursor.execute(sql, (attendance_date,))
+        records = cursor.fetchall()
+
+    else:
+
+        sql = """
+        SELECT
+            students.student_name,
+            students.roll_number,
+            attendance.attendance_date,
+            attendance.status
+        FROM attendance
+        INNER JOIN students
+        ON attendance.student_id = students.id
+        ORDER BY attendance.attendance_date DESC
+        """
+
+        cursor.execute(sql)
+        records = cursor.fetchall()
+
+    return render_template("reports.html", records=records)
+
+@app.route("/export_excel")
+def export_excel():
+
+    sql = """
+    SELECT
+        students.student_name,
+        students.roll_number,
+        attendance.attendance_date,
+        attendance.status
+    FROM attendance
+    INNER JOIN students
+    ON attendance.student_id = students.id
+    ORDER BY attendance.attendance_date DESC
+    """
+
+    cursor.execute(sql)
+    records = cursor.fetchall()
+
+    df = pd.DataFrame(
+        records,
+        columns=[
+            "Student Name",
+            "Roll Number",
+            "Date",
+            "Status"
+        ]
+    )
+
+    filename = "Attendance_Report.xlsx"
+
+    df.to_excel(filename, index=False)
+
+    return send_file(
+        filename,
+        as_attachment=True
+    )
+
+@app.route("/export_pdf")
+def export_pdf():
+
+    sql = """
+    SELECT
+        students.student_name,
+        students.roll_number,
+        attendance.attendance_date,
+        attendance.status
+    FROM attendance
+    INNER JOIN students
+    ON attendance.student_id = students.id
+    ORDER BY attendance.attendance_date DESC
+    """
+
+    cursor.execute(sql)
+    records = cursor.fetchall()
+
+    data = [["Student Name", "Roll No", "Date", "Status"]]
+
+    for row in records:
+        data.append(list(row))
+
+    filename = "Attendance_Report.pdf"
+
+    pdf = SimpleDocTemplate(filename)
+
+    table = Table(data)
+
+    table.setStyle(TableStyle([
+        ("BACKGROUND", (0, 0), (-1, 0), colors.grey),
+        ("TEXTCOLOR", (0, 0), (-1, 0), colors.whitesmoke),
+        ("GRID", (0, 0), (-1, -1), 1, colors.black),
+        ("BACKGROUND", (0, 1), (-1, -1), colors.beige),
+        ("ALIGN", (0, 0), (-1, -1), "CENTER"),
+        ("BOTTOMPADDING", (0, 0), (-1, 0), 12),
+    ]))
+
+    pdf.build([table])
+
+    return send_file(filename, as_attachment=True)
+
 @app.route("/delete_student/<int:id>")
 def delete_student(id):
 
